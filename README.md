7c16644b22183ec4bb7ff8a4606b6c88886a1cc0
bokther <boktherahmed@gmail.com>
2022-04-24 16:23:52 -0400

    added color coding

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# are-you-balanced
+# AngularJS String Balancer Application
+
+Requirements:
+- Node.js
+
+To Run:
+1. npm install
+2. npm start
+3. Navigate to http://localhost:8000/ in your browser
--- app/validator/validator.component.js
+++ app/validator/validator.component.js
@@ -1,4 +1,6 @@
 function ValidatorController() {
+    
+    // Default pairs
     this.pairs = {
         "(": ")",
         "[": "]",
@@ -9,13 +11,15 @@ function ValidatorController() {
     this.newClose = "";
     this.validationMessage = "";
     this.stringToBalance = "";
-    this.result = "";
+    this.result="";
+    this.colorCoded = [];
 
     this.addPair = function () {
         if (this.isValidPair()) {
             this.pairs[this.newOpen] = this.newClose;
             this.newOpen = "";
             this.newClose = "";
+            this.validationMessage = ""; // Clear the validation
         }
         else {
             this.validationMessage = "The new open or close value already exists in the pairs."
@@ -27,39 +31,70 @@ function ValidatorController() {
     }
 
     this.isValidPair = function () {
+        // A pair is valid if the opening character is not in the list of keys
+        // and if the closing character is not in the list of values
         return !this.pairs.hasOwnProperty(this.newOpen) && !Object.values(this.pairs).includes(this.newClose);
     }
 
-    var isBalanced = function (str, pairs) {
-
+    this.checkBalance = function () {
+        // Default the characters to be grey
+        this.colorCoded = this.stringToBalance.split('').map(c => ({ character: c, color: 'grey' }));
+        // Stack that stores the [characer, index] of characters that need to be closed
+        // [0] -> character
+        // [1] -> index of character in this.colorCoded
         let needsClosing = [];
 
-        for (var i = 0; i < str.length; i++) {
-            if (pairs.hasOwnProperty(str[i])) {
-                needsClosing.push(str[i]);
+        // We loop through the input text and match opening characters with closing characters
+        // We are also updating the color value of the characters
+        for (var i = 0; i < this.stringToBalance.length; i++) {
+            // If an opening character is found, it is pushed onto the stack
+            if (this.pairs.hasOwnProperty(this.stringToBalance[i])) {
+                needsClosing.push([this.stringToBalance[i], i]);
             }
-            else if (Object.values(pairs).includes(str[i])) {
+            // Else, if a closing character is found, we attempt to match to the corresponding 
+            // opening character in the stack
+            else if (Object.values(this.pairs).includes(this.stringToBalance[i])) {
                 if (needsClosing.length === 0) {
+                    this.colorCoded[i].color = 'red';
                     return false;
                 }
                 else {
-                    if (pairs[needsClosing.pop()] != str[i]) {
-                        return false
+                    let temp = needsClosing.pop();
+                    if (this.pairs[temp[0]] != this.stringToBalance[i]) {
+                        this.colorCoded[temp[1]].color = 'red';
+                        this.colorCoded[i].color = 'red';
+                        return false;
+                    }
+                    else {
+                        this.colorCoded[temp[1]].color = 'green';
+                        this.colorCoded[i].color = 'green';
                     }
                 }
             }
+            else {
+                // For charcaters that aren't defined in pairs
+                this.colorCoded[i].color = 'black';
+            }
         }
 
-        return needsClosing.length === 0;
+        if (needsClosing.length !== 0) {
+            // In the end, if the stack isn't empty, we update the color of the remaining items 
+            needsClosing.forEach(x => this.colorCoded[x[1]].color = 'red');
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 
     this.validate = function () {
-        this.result = isBalanced(this.stringToBalance, this.pairs) ? "It is balanced!" : "It is not balanced";
+        this.result = this.checkBalance() ? "The text is balanced!" : "The text is not balanced :(";
     }
 }
 
 angular.module('areYouBalancedApp').component('validator', {
     templateUrl: 'validator/validator.template.html',
     controller: [
-        ValidatorController]
+        ValidatorController
+    ]
 });
